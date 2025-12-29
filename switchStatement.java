bec2050bfdce114d9466606fe2522ff205d405df
blacktutor97zamani <yussifmohammedc55@gmail.com>
2025-12-29 08:33:19 -0800

    switchStatement

---
+++ switchStatement.java
@@ -0,0 +1,15 @@
+public class switchStatement {
+    /*
+    *Java has updated its switch statements from the previous method to
+    * a new easy and fast way without the use of break in between cases.
+    */
+
+    public static void main(String[] args){
+        String days = "Wednesday";
+
+        //Previous method
+        switch (days){
+
+        }
+    }
+}
