e896a14386a65f931e345341dbe4a28c7d3d9b0c
blacktutor97zamani <yussifmohammedc55@gmail.com>
2025-12-31 11:55:11 -0800

    creating the addition and subtraction methods in the switch cases

--- simpleCalculator.java
+++ simpleCalculator.java
@@ -1,3 +1,79 @@
+import java.util.Scanner;
+
 public class simpleCalculator {
+    /*
+    1. create methods for
+    - Addition
+    - Minus
+    - Division
+    - Multiplication
+    2. Take values from user
+     */
+
+    public static int sum(int... numbers){
+        int add = 0;
+        for(int n : numbers){
+            add += n;
+        }
+
+        return add;
+    }
+
+    public static void main(String[] args){
+
+        int result = 0;
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        System.out.print("Enter number of Digits: ");
+        int num = scanner.nextInt();
+        scanner.nextLine();
+
+        int[] numbers = new int[num];
+
+        System.out.print("Enter character (+, -, /, *): ");
+        char character = scanner.nextLine().charAt(0);
+
+        switch(character){
+            // *******************ADDITION*****************
+            case '+':
+                for(int i = 0; i < num; i++){
+                    System.out.print("Enter number: ");
+                    numbers[i] = scanner.nextInt();
+
+                   // result = numbers[0];
+                    result += numbers[i];
+
+                    break;
+                }
+                System.out.println(result);
+
+                // *******************SUBTRACTION*****************
+
+            case '-':
+                for(int i = 0; i < num; i++){
+                    System.out.print("Enter number: ");
+                    numbers[i] = scanner.nextInt();
+
+                    //result += numbers[i];
+                }
+
+                result = numbers[0];
+                for(int i = 1; i < num; i++){
+                    //System.out.print(j + " ");
+                    result -= numbers[i];
+
+                }
+                System.out.println("Result: " + result);
+              //System.out.println("Result Value: "+ result);
+
+
+
+        }
+
+
+
+    }
 
 }
