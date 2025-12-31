bd2197695906903a386af73c9f735d311f8108fc
blacktutor97zamani <yussifmohammedc55@gmail.com>
2025-12-31 10:28:46 -0800

    simpleCalculator

--- classAndObject.java
+++ classAndObject.java
@@ -1,3 +1,17 @@
 public class classAndObject {
+    public class calc{
+        int a;
+        int b;
+         public int add(int a, int b){
+             return a +  b;
+         }
+    }
 
+
+    public static void main(String[] args) {
+
+
+    }
 }
+
+
