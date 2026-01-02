50aa1786650b14351dce36d597b880b0f221706d
YUSSIF KONDA MOHAMMED <71007598+blacktutor97zamani@users.noreply.github.com>
2026-01-02 12:25:13 -0800

    Refactor simple Calculator for better readability

--- simpleCalculator.java
+++ simpleCalculator.java
@@ -45,11 +45,13 @@ public class simpleCalculator {
                    // result = numbers[0];
                     result += numbers[i];
 
-                    break;
+                    
                 }
                 System.out.println(result);
 
-                // *******************SUBTRACTION*****************
+                break;
+
+                //***************************************SUBTRACTION*****************************************
 
             case '-':
                 for(int i = 0; i < num; i++){
@@ -68,7 +70,43 @@ public class simpleCalculator {
                 System.out.println("Result: " + result);
               //System.out.println("Result Value: "+ result);
 
+                break;
 
+
+                //*******************************DIVISION**************************************
+            case '/':
+                for(int i = 0; i < num; i++){
+                    System.out.println("Enter number: ");
+                    numbers[i] = scanner.nextInt();
+                }
+
+                result = numbers[0];
+                for(int i =1; i < num; i++){
+                    if(numbers[i] == 0){
+                        System.out.println("Cannot divide by Zero");
+                        return;
+                    }
+                    result /= numbers[i];
+                }
+                System.out.println("Result: " + result);
+                break;
+
+            //**************************************MULTIPLICATION********************************************
+            case '*':
+                result = 1;
+                for(int i = 0; i < num; i++){
+                    System.out.print("Enter number: ");
+                    numbers[i] = scanner.nextInt();
+                    result *= numbers[i];
+                }
+
+                System.out.println("Result: " + result);
+                break;
+
+                
+            //*******************************DEFAULT***************************************
+            default:
+                System.out.println("Invalid Operator");
 
         }
 
