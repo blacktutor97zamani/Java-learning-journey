17e2ba2924a19f026e279169e53eee20d2f1f8b1
blacktutor97zamani <yussifmohammedc55@gmail.com>
2026-01-04 02:11:55 -0800

    arrays

---
+++ arrays.java
@@ -0,0 +1,19 @@
+public class arrays {
+    public static void main(String[] ag){
+
+        int[] arr = new int[5];
+       // int fill = (int)(Math.random()*10);
+        System.out.println(fill);
+        for(int i = 0; i < 5; i++){
+            arr[i] = (int)(Math.random()*10);;
+            int store = arr[i];
+
+        }
+
+        for(int i = 1; i < 5; i++){
+            System.out.println(arr[i]);
+        }
+
+
+    }
+}
