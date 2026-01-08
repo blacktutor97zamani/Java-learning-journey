2b7b3860c15e433bccd86ee424c551530ff90801
blacktutor97zamani <yussifmohammedc55@gmail.com>
2026-01-08 10:05:09 -0800

    More about String class

--- javaString.java
+++ javaString.java
@@ -1,3 +1,11 @@
 public class javaString {
-
+    public static void main(String[] args){
+        //string is a class rather than than primitive data type.
+        // it creates an object in two different ways.
+        //either by declaring it like a variable just like the primitive data type or
+        //creating an object like below.
+        //the string class consists of other methods in the class. ex.. hashCode, charAt(), toLowerCase etc.
+    String name = new String("Yussif");
+        System.out.println(name.hashCode());
+}
 }
