cf903a849e0fd08731844c1a664292dac8a247c9
YUSSIF KONDA MOHAMMED <71007598+blacktutor97zamani@users.noreply.github.com>
2026-01-12 19:56:04 +0000

    Create recursion.java

---
+++ recursion.java
@@ -0,0 +1,11 @@
+public class recursion{
+//Recursion in java is when a method or code 
+//calls itself. This reduces repeatition if processes and
+//speedens production. 
+
+//creating a recursive method 
+
+    public static void main(String[] args) {
+
+}
+}
