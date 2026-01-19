53baaffb6079a28f0d45041a623b4990439a5ffc
blacktutor97zamani <yussifmohammedc55@gmail.com>
2026-01-19 15:13:41 -0800

    Java Constructors

---
+++ Student.java
@@ -0,0 +1,30 @@
+public class Student {
+    //This lesson is based on Java constructors
+    String name = "Kwame";
+    int year = 2022;
+    String Course = "Computer Science";
+
+    //with the above declared class variable, any instance of the class we create
+    // adopts same variables unless we create a constructor.
+
+    Student(String a, int b, String c){
+        this.name = a;
+        this.year = b;
+        this.Course = c;
+    }
+
+    public static void main(String[] a){
+
+        //All the instances(student 1 and 2) created have same values.
+        //Second edit: Created a constructor and added parameters to pass different arguments.
+        Student student1 = new Student("Yussif", 2002, "General Arts");
+       // student1.name;
+        //student1.Course;
+        System.out.println(student1.name + "\n" + student1.year);
+
+        Student student2 = new Student("Mohammed", 2014, "General Science");
+        // student1.name;
+        //student1.Course;
+        System.out.println(student2.name + "\n" + student2.year);
+    }
+}
